f8ab15006ce2bb2907e8f4d7be6bf4829eb2ae40
pramesh101 <prameshsingh2411@gmail.com>
2021-05-22 12:19:40 +0530

    google map integrated.

--- app/src/main/java/com/famco/trainingproject1/views/base/BaseFragment.kt
+++ app/src/main/java/com/famco/trainingproject1/views/base/BaseFragment.kt
@@ -2,16 +2,35 @@ package com.famco.trainingproject1.views.base
 
 import android.app.AlertDialog
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+
 
 open class BaseFragment : Fragment() {
-    fun showToast(context: Context,message:String){
-        Toast.makeText(context, ""+message, Toast.LENGTH_SHORT).show()
+    fun showToast(context: Context, message: String) {
+        Toast.makeText(context, "" + message, Toast.LENGTH_SHORT).show()
     }
-    fun showAlertDialog(context: Context,message: String){
-        val builder:AlertDialog.Builder=AlertDialog.Builder(context)
+
+    fun showAlertDialog(context: Context, message: String) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(context)
         builder.setMessage(message)
         builder.show()
+    }
+
+    open fun getBitmapFromVectorDrawable(context: Context, drawableId: Int): BitmapDescriptor {
+        val drawable = ContextCompat.getDrawable(context, drawableId)
+        val bitmap = Bitmap.createBitmap(
+            drawable!!.intrinsicWidth,
+            drawable.intrinsicHeight, Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(bitmap)
+        drawable.setBounds(0, 0, canvas.width, canvas.height)
+        drawable.draw(canvas)
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
     }
 }
---
+++ app/src/main/java/com/famco/trainingproject1/views/main/fragment/MapsFragment.kt
@@ -0,0 +1,57 @@
+package com.famco.trainingproject1.views.main.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.famco.trainingproject1.R
+import com.famco.trainingproject1.views.base.BaseFragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapsFragment : BaseFragment(), GoogleMap.OnMarkerClickListener {
+ private var mMarker: Marker?=null
+    private val callback = OnMapReadyCallback { googleMap ->
+        val sydney = LatLng(-34.0, 151.0)
+
+        mMarker=googleMap.addMarker( MarkerOptions().position(sydney).title("Marker in Sydney").icon(
+            context?.let { getBitmapFromVectorDrawable(it,R.drawable.ic_car) }
+        ))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_maps, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment: SupportMapFragment? =
+            childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+    }
+    fun setMapLongClick(map:GoogleMap){
+        map.setOnMapClickListener {
+
+        }
+    }
+
+    override fun onMarkerClick(marker: Marker): Boolean {
+        if(marker == mMarker) {
+            Toast.makeText(context, "marker clicked" + marker.position, Toast.LENGTH_SHORT).show()
+        }
+        return true
+    }
+}
